6c49470593c9d965e724bd6ef93f5c7ad994d80a
Antonio Jose Iniesta <iniesta@mxgteam.com>
2015-08-06 19:13:48 +0200

    Sample WordCount MapReduce
    Adding the Maven project and a first WordCount MapReduce project

---
+++ src/main/java/com/iniesta/hdp/test/WordCount.java
@@ -0,0 +1,46 @@
+package com.iniesta.hdp.test;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class WordCount {
+
+	public static void main(String[] args) throws Exception{
+		Path inputPath = new Path(args[0]);
+		Path outputPath = new Path(args[1]);
+		
+		Configuration conf = new Configuration(true);
+		
+		Job job = new Job(conf, "my wordcount mapreduce");
+		job.setJarByClass(WordCountMapper.class);
+		
+		job.setMapperClass(WordCountMapper.class);
+		job.setReducerClass(WordCountReducer.class);
+		job.setNumReduceTasks(1);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(job, inputPath);
+		job.setInputFormatClass(TextInputFormat.class);
+		
+		FileOutputFormat.setOutputPath(job, outputPath);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		FileSystem hdfs = FileSystem.get(conf);
+		if(hdfs.exists(outputPath)){
+			hdfs.delete(outputPath, true);
+		}
+		
+		int code = job.waitForCompletion(true) ? 0 : 1;
+		System.exit(code);
+	}
+}
---
+++ src/main/java/com/iniesta/hdp/test/WordCountReducer.java
@@ -0,0 +1,23 @@
+package com.iniesta.hdp.test;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+	@Override
+	protected void reduce(Text text, Iterable<IntWritable> values,
+			Reducer<Text, IntWritable, Text, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+		int sum = 0;
+		for (IntWritable value : values) {
+			sum += value.get();
+		}
+		context.write(text, new IntWritable(sum));
+	}
+
+	
+}
