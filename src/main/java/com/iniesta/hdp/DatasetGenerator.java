9998125d3fe784bee48b407b522f1e9d8914be1c
Antonio J. Iniesta <antonioj.iniesta@gmail.com>
2015-10-24 17:15:17 +0200

    Full version of the categories data generator

--- src/main/java/com/iniesta/hdp/DatasetGenerator.java
+++ src/main/java/com/iniesta/hdp/DatasetGenerator.java
@@ -3,93 +3,108 @@ package com.iniesta.hdp;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Locale;
 import java.util.Random;
 
 public abstract class DatasetGenerator {
 
 	private int numFiles;
 	private Random random;
+	private DecimalFormat formatter;
 
 	public DatasetGenerator(String[] args) {
-		if(args.length!=1) {
-            numFiles = 1;
-        } else{
-        	numFiles = Integer.parseInt(args[0]);
-        }
-        random = new Random(System.currentTimeMillis());
+		if (args.length != 1) {
+			numFiles = 1;
+		} else {
+			numFiles = Integer.parseInt(args[0]);
+		}
+		random = new Random(System.currentTimeMillis());
+		DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.ENGLISH);
+		formatter = new DecimalFormat("#.##", symbols);
 	}
 
 	public void generateFiles() throws Exception {
 		for (int i = 0; i < numFiles; i++) {
-			generateFile(i);			
+			generateFile(i);
 		}
 	}
-	
+
 	public void generateFile(int i) throws Exception {
-        BufferedWriter bw = new BufferedWriter(new FileWriter("data_"+i+".csv"));
-        for(int k = 0; k < getNumLines() ; k++ ) {
-            bw.write(generateLine()+"\n");
-        }
-        bw.flush();
-        bw.close();
-    }
+		BufferedWriter bw = new BufferedWriter(new FileWriter("data_" + i + ".csv"));
+		for (int k = 0; k < getNumLines(); k++) {
+			bw.write(getLine() + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
 
 	public String getRandomDate() {
 
-        int year = random.nextInt(55)+1960;
-        int month = random.nextInt(12)+1;
-        int day;
-        if(month==2){
-            if((year % 4)==0){
-                day = random.nextInt(29) + 1;
-            }else {
-                day = random.nextInt(28) + 1;
-            }
-        } else if(month==4 || month == 6 || month==9 || month==11){
-            day = random.nextInt(30)+1;
-        } else {
-            day = random.nextInt(31)+1;
-        }
-
-        return year + "/" + fillZero(month) + "/" + fillZero(day);
-    }
-
-	public String getRandomDouble(double max){
-		double rand = random.nextDouble()*max;
-		BigDecimal bd = new BigDecimal(rand);
-		return bd.toString();
+		return getRandomDate(1960);
 	}
-	
-	public String getRandomInt(int max){
+
+	public String getRandomDate(int startingYear) {
+
+		Calendar c = new GregorianCalendar();
+		int currentYear = c.get(Calendar.YEAR);
+
+		int year = random.nextInt(currentYear - startingYear) + startingYear;
+		int month = random.nextInt(12) + 1;
+		int day;
+		if (month == 2) {
+			if ((year % 4) == 0) {
+				day = random.nextInt(29) + 1;
+			} else {
+				day = random.nextInt(28) + 1;
+			}
+		} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+			day = random.nextInt(30) + 1;
+		} else {
+			day = random.nextInt(31) + 1;
+		}
+
+		return year + "/" + fillZero(month) + "/" + fillZero(day);
+	}
+
+	public String getRandomDouble(double max) {
+		double rand = random.nextDouble() * max;
+		return formatter.format(rand);
+	}
+
+	public String getRandomInt(int max) {
 		return new BigDecimal(random.nextInt(max)).toPlainString();
 	}
-	
-	public String getRandomString(String[] options){
+
+	public String getRandomString(String[] options) {
 		int index = random.nextInt(options.length);
 		return options[index];
 	}
-	
+
 	private String fillZero(int value) {
 		String str = value + "";
-		if(value <= 9){
+		if (value <= 9) {
 			str = "0" + value;
 		}
 		return str;
 	}
 
-	public String getLine(){
+	public String getLine() {
 		String sLine = "";
 		List<String> lLine = generateLine();
 		for (int i = 0; i < lLine.size(); i++) {
 			sLine += lLine.get(i);
-			if(i<(lLine.size()-1)){
+			if (i < (lLine.size() - 1)) {
 				sLine += getFieldSeparator();
 			}
 		}
 		return sLine;
 	}
-	
+
 	private String getFieldSeparator() {
 		return ",";
 	}
@@ -97,5 +112,5 @@ public abstract class DatasetGenerator {
 	public abstract List<String> generateLine();
 
 	public abstract int getNumLines();
-	
+
 }
--- src/main/java/com/iniesta/hdp/categories/GeneratorData.java
+++ src/main/java/com/iniesta/hdp/categories/GeneratorData.java
@@ -12,7 +12,7 @@ import com.iniesta.hdp.DatasetGenerator;
  */
 public class GeneratorData extends DatasetGenerator{
 
-	private static String[] concepts = { "gas", "electricity", "internet", "car", "groceries", "computer" };
+	private static String[] concepts = { "gas", "electricity", "internet", "car", "groceries", "computer", "rent", "taxes" };
 	
 	public GeneratorData(String[] args) {
 		super(args);
@@ -25,7 +25,7 @@ public class GeneratorData extends DatasetGenerator{
 	@Override
 	public List<String> generateLine() {
 		List<String> line = new ArrayList<String>();
-		line.add(getRandomDate());
+		line.add(getRandomDate(2010));
 		line.add(getRandomString(concepts));
 		line.add(getRandomDouble(1000D));
 		return line;
